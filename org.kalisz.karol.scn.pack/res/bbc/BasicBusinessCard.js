f0302723eac4a53ef9e0e041ae07a46c3588c869
Karol Kalisz <karol@kalisz.org>
2014-09-12 22:59:53 +0200

    Adding BasicBusinessCard

---
+++ org.kalisz.karol.scn.pack/res/bbc/BasicBusinessCard.js
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2014 Karol Kalisz
+ * 
+ * Original Source Code Location:
+ *  https://github.com/KarolKalisz/DesignStudioSdkPack
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
+jQuery.sap.require("sap.ui.commons.layout.AbsoluteLayout");
+
+var org_kalisz_karol_scn_pack_BasicBusinessCard_resourcePrefix = "res/bbc/";
+var org_kalisz_karol_scn_pack_BasicBusinessCard_lastScriptCalled = $("script:last");
+var org_kalisz_karol_scn_pack_BasicBusinessCard_fullUrlOfLastScriptCalled = org_kalisz_karol_scn_pack_BasicBusinessCard_lastScriptCalled.attr("src");
+var org_kalisz_karol_scn_pack_BasicBusinessCard_cutIndexOnMainEntryPoint = org_kalisz_karol_scn_pack_BasicBusinessCard_fullUrlOfLastScriptCalled.indexOf(org_kalisz_karol_scn_pack_BasicBusinessCard_resourcePrefix);
+var org_kalisz_karol_scn_pack_BasicBusinessCard_accessUrlForRes = org_kalisz_karol_scn_pack_BasicBusinessCard_fullUrlOfLastScriptCalled.substring(0, org_kalisz_karol_scn_pack_BasicBusinessCard_cutIndexOnMainEntryPoint) + org_kalisz_karol_scn_pack_BasicBusinessCard_resourcePrefix;
+
+sap.ui.commons.layout.AbsoluteLayout.extend("org.kalisz.karol.scn.pack.BasicBusinessCard", {
+
+	initDesignStudio: function() {
+		var that = this;
+		
+		this.addStyleClass("scn-pack-BasicBusinessCard-Card");
+		
+		this._lNameLink = new sap.ui.commons.Link();
+
+		this._lNameLink.attachBrowserEvent('click', function() {
+			that.fireDesignStudioEvent("onPress");
+			}
+		);
+		
+		this._lNameLink.addStyleClass("scn-pack-BasicBusinessCard-NameLink");
+
+		this.addContent(
+				this._lNameLink,
+				{left: "42px", top: "2px"}
+		);
+
+		this._lImage = new sap.ui.commons.Image({
+			width: "32px",
+			height: "32px"
+			}
+		);
+
+		this._lImage.addStyleClass("scn-pack-BasicBusinessCard-Image");
+		
+		this.addContent(
+				this._lImage,
+				{left: "5px", top: "5px"}
+		);
+
+		this._lText = new sap.ui.commons.TextView();
+
+		this._lText.addStyleClass("scn-pack-BasicBusinessCard-Title");
+		
+		this.addContent(
+				this._lText,
+				{left: "42px", top: "20px"}
+		);
+	},
+	
+	setImage : function(value) {
+		if(this._Image == value) {
+			return;
+		} else {
+			this._Image = value;
+		}
+	},
+
+	getImage : function() {
+		return this._Image;
+	},
+	
+	setName : function(value) {
+		if(this._Name == value) {
+			return;
+		} else {
+			this._Name = value;
+		}
+	},
+
+	getName : function() {
+		return this._Name;
+	},
+
+	setTitle : function(value) {
+		if(this._Title == value) {
+			return;
+		} else {
+			this._Title = value;
+		}
+	},
+
+	getTitle : function() {
+		return this._Title;
+	},
+
+	setImage : function(value) {
+		if(this._Image == value) {
+			return;
+		} else {
+			this._Image = value;
+		}
+	},
+
+	getImage : function() {
+		return this._Image;
+	},
+
+	renderer: {},
+	
+	afterDesignStudioUpdate : function() {
+		var that = this;
+
+		this._lNameLink.setText (this.getName());
+		this._lNameLink.setTooltip (this.getName());
+
+		this._lImage.setSrc (this.getImage());
+
+		this._lText.setText (this.getTitle());
+	}
+});
